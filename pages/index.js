5b4943f42253a04d7804de8b38f9915ad32d5ccc
justin <>
2021-08-08 18:08:39 +0800

    feat: add header component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import Header from "../components/Header";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Hello</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  )
+    return (
+        <div className="">
+            <Head>
+                <title>Airbnb</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            {/*Header*/}
+            <Header/>
+            {/*Banner*/}
+        </div>
+    )
 }
