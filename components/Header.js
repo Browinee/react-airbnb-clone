f4b83a09bf817c8a15638876e34ca30e43fdc00d
justin <>
2021-08-09 17:48:34 +0800

    feat: add query

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,13 @@
 import {useState} from "react";
 import Image from "next/image";
-import {GlobeAltIcon, UserIcon, MenuIcon, SearchIcon, UserCircleIcon} from "@heroicons/react/solid";
+import {GlobeAltIcon, MenuIcon, SearchIcon, UserCircleIcon, UserIcon} from "@heroicons/react/solid";
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import {DateRangePicker} from 'react-date-range';
 import {useRouter} from "next/router";
 
 function Header(props) {
+    const {placeHolder} = props;
     const router = useRouter();
     const goHome = () => {
         router.push("/");
@@ -28,7 +29,7 @@ function Header(props) {
     const [numberOfGuests, setNumberOfGuests] = useState(1);
     const numberOfGuestsHandler = e => {
         const value = +e.target.value;
-        if(+value < 1 || isNaN(+value)) {
+        if (+value < 1 || isNaN(+value)) {
             setNumberOfGuests(1);
             return
         }
@@ -39,11 +40,19 @@ function Header(props) {
         setSearch("");
     };
     const submitHandler = () => {
-       router.push("/search");
+        router.push({
+            pathname: '/search',
+            query: {
+                location: search,
+                startDate: selectionRange.startDate.toISOString(),
+                endDate: selectionRange.endDate.toISOString(),
+                numberOfGuests,
+            }
+        });
     }
     return (
         <header className="sticky top-0 z-50  bg-white grid grid-cols-3 shadow-md p-5 md:px-10">
-            <div onClick={goHome}className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div onClick={goHome} className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image
                     src="https://links.papareact.com/qd3"
                     layout={"fill"}
@@ -53,9 +62,10 @@ function Header(props) {
             </div>
             <div className={"flex items-center md:border-2 rounded-full py-2 md:shadow-sm"}>
                 <input
+                    placeholder={placeHolder || "Stay your search"}
                     value={search}
                     onChange={searchHandler}
-                    type="text" placeholder={"Start your search"}
+                    type="text"
                     className={"flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"}/>
                 <SearchIcon
                     className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
@@ -77,7 +87,8 @@ function Header(props) {
                         <div className={"flex items-center border-b mb-4"}>
                             <h2 className={"text-2xl font-semibold flex-grow"}>Number of Guests</h2>
                             <UserIcon className={"h-5"}/>
-                            <input min={1} type="number" value={numberOfGuests} onChange={numberOfGuestsHandler} className={"w-12 pl-2 text-lg outline-none text-red-400"} />
+                            <input min={1} type="number" value={numberOfGuests} onChange={numberOfGuestsHandler}
+                                   className={"w-12 pl-2 text-lg outline-none text-red-400"}/>
                         </div>
                         <div className={"flex"}>
                             <button className={"flex-grow text-gray-500"} onClick={resetHandler}>Cancel</button>
--- pages/search.js
+++ pages/search.js
@@ -1,28 +1,38 @@
 import Header from "../components/Header";
 import Head from "next/head";
+import {useRouter} from "next/router";
+import {format} from "date-fns";
 import Footer from "../components/Footer";
 
-function Search(props) {
+const formattedDate = (date) =>{
+    if(!date) return "";
+    return format(new Date(date), "dd MMM yy");
+}
+
+function Search(){
+    const router = useRouter();
+    const {location, startDate, endDate, numberOfGuests} = router.query;
+    const range = `${formattedDate(startDate)} - ${formattedDate(endDate)}`;
     return (
         <div>
             <Head>
                 <title>Search</title>
             </Head>
-            <Header />
-            <main className={"flex"} >
-               <section className={"flex-grow pt-14 px-6"}>
-                   <p className={"text-xs"}> 300+ Stays - for 5 number of guests</p>
-                   <h1 className="text-3xl font-semibold mt-2 mb-6"> Stays in New Yourk</h1>
-                   <div className={"hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap"}>
-                       <p className={"button"}> Cancellation Flexibility</p>
-                       <p className={"button"}>Type of Place</p>
-                       <p className={"button"}>Price</p>
-                       <p className={"button"}>Rooms and Beds</p>
-                       <p className={"button"}>More filters</p>
-                   </div>
-               </section>
+            <Header placeHolder={`${location} | ${range} | ${numberOfGuests}`} />
+            <main className={"flex"}>
+                <section className={"flex-grow pt-14 px-6"}>
+                    <p className={"text-xs"}> 300+ Stays - {range} - for {numberOfGuests} guests</p>
+                    <h1 className="text-3xl font-semibold mt-2 mb-6"> Stays in {location}</h1>
+                    <div className={"hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap"}>
+                        <p className={"button"}> Cancellation Flexibility</p>
+                        <p className={"button"}>Type of Place</p>
+                        <p className={"button"}>Price</p>
+                        <p className={"button"}>Rooms and Beds</p>
+                        <p className={"button"}>More filters</p>
+                    </div>
+                </section>
             </main>
-            <Footer />
+            <Footer/>
         </div>
     );
 }
