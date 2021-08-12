b30eac0d0470f41830f19280ab8efd81e14e7b07
justin <>
2021-08-12 19:08:16 +0800

    feat: modify search

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,10 @@
 import Header from "../components/Header";
-import Head from "next/head";
 import {useRouter} from "next/router";
 import {format} from "date-fns";
 import Footer from "../components/Footer";
 import InfoCard from "../components/InfoCard";
+import Map from "../components/Map";
+import AppHead from "../components/atom/AppHead";
 
 const formattedDate = (date) => {
     if (!date) return "";
@@ -15,14 +16,16 @@ function Search(props) {
     const router = useRouter();
     const {location, startDate, endDate, numberOfGuests} = router.query;
     const range = `${formattedDate(startDate)} - ${formattedDate(endDate)}`;
+
+
     return (
         <div>
-            <Head>
-                <title>Search</title>
-            </Head>
+            <AppHead/>
             <Header placeHolder={`${location} | ${range} | ${numberOfGuests}`}/>
-            <main className={"flex"}>
-                <section className={"flex-grow pt-14 px-6"}>
+            <main
+                className={"lg:grid-cols-[700px,1fr] xl:grid-cols-[840px,1fr] flex-grow grid grid-cols-1  duration-500"}
+            >
+                <section className={"px-4 py-8 duration-500 lg:py-12 lg:px-7"}>
                     <p className={"text-xs"}> 300+ Stays - {range} - for {numberOfGuests} guests</p>
                     <h1 className="text-3xl font-semibold mt-2 mb-6"> Stays in {location}</h1>
                     <div className={"hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap"}>
@@ -32,16 +35,23 @@ function Search(props) {
                         <p className={"button"}>Rooms and Beds</p>
                         <p className={"button"}>More filters</p>
                     </div>
+                    <div className={"flex flex-col"}>
+                        {
+                            searchResults.map((item) => {
+                                const {img, location, title, description, star, price, total, long, lat} = item;
+                                return <InfoCard key={img} img={img} location={location} title={title}
+                                                 description={description} star={star} price={price} total={total}
+                                                 lat={lat} long={long}/>
+                            })
+                        }
+                    </div>
+                </section>
+                <section
+                    className={` sm:block sm:sticky top-[86px] h-map flex-grow bg-yellow-900 bg-opacity-10 duration-100`}
+                >
+                    <Map searchResults={searchResults}/>
                 </section>
             </main>
-            <div className={"flex flex-col"}>
-                {
-                    searchResults.map((item) => {
-                        const {img, location, title, description, star, price, total, long, lat} = item;
-                        return <InfoCard  key={img} img={img} location={location} title={title} description={description} star={star} price={price} total={total} lat={lat} long={long}/>
-                    })
-                }
-            </div>
             <Footer/>
         </div>
     );
@@ -49,11 +59,9 @@ function Search(props) {
 
 export default Search;
 
-const SEARCH_URL = "https://links.papareact.com/isz";
-
 export async function getServerSideProps() {
     let result;
-    const searchResults = await fetch(SEARCH_URL);
+    const searchResults = await fetch(process.env.SEARCH_URL);
     result = !searchResults.ok
         ? []
         : await searchResults.json();
