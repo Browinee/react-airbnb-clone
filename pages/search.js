7ab57b25346a93a8a834abc105796e66a86f70d2
justin <>
2021-08-12 20:04:58 +0800

    feat: fix url

--- pages/search.js
+++ pages/search.js
@@ -61,7 +61,7 @@ export default Search;
 
 export async function getServerSideProps() {
     let result;
-    const searchResults = await fetch("https://links.papareact.com/is");
+    const searchResults = await fetch("https://links.papareact.com/isz");
     result = !searchResults.ok
         ? []
         : await searchResults.json();
